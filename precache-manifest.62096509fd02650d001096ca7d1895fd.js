d9b622a8dfab4e379490da98362a7e77470626f1
Kari Burt <kandreahburt@gmail.com>
2020-04-01 15:12:12 -0500

    Updates

---
+++ precache-manifest.62096509fd02650d001096ca7d1895fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ad74a6e32c225d31e178cf2df477c5",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "979f230d2ba3a5823a74",
+    "url": "/face-recognition-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8b7916cfd73fb18fb47c",
+    "url": "/face-recognition-brain/static/css/main.36ba169b.chunk.css"
+  },
+  {
+    "revision": "979f230d2ba3a5823a74",
+    "url": "/face-recognition-brain/static/js/2.f9aa3acc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.f9aa3acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7916cfd73fb18fb47c",
+    "url": "/face-recognition-brain/static/js/main.1b3a9afc.chunk.js"
+  },
+  {
+    "revision": "7004dc07691cc6705258",
+    "url": "/face-recognition-brain/static/js/runtime-main.fbadeeaf.js"
+  }
+]);
